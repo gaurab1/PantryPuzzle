5352bf5eadd0cf8ea63e0e80d7f0325b3b236eba
mna1024 <maxiattiogbe@gmail.com>
2023-09-16 18:36:13 -0400

    Added upload images functionality

--- react-crud/src/components/create.js
+++ react-crud/src/components/create.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Form } from 'semantic-ui-react'
 
 import foodSubmit from '../handles/foodsubmit';
@@ -19,11 +19,37 @@ export default function Create() {
 
     const [food, setFood] = useState('');
     const [expiry, setExpiry] = useState('');
+    const [images, setImages] = useState([]);
+    const [imageURLs, setImageURLs] = useState([]);
     const postData = () => {
         foodSubmit(food, expiry)
     }
+
+    useEffect(() => {
+        if (images.length < 1) return;
+        const newImageUrls = [];
+        images.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
+        setImageURLs(newImageUrls);
+    }, [images]);
+
+    function onImageChange(e){
+        setImages([...e.target.files]);
+    }
+
     return (
         <div>
+            Upload Food Images
+            <br/>
+            <br/>
+            <input type="file" multiple accept="image/*" onChange={onImageChange} />
+            {imageURLs.map(imageSrc => <img src={imageSrc}  width="10%"/>)}
+            <br/>
+            <br/>
+
+            OR
+
+            <br/>
+            <br/>
             <Form className="create-form">
                 <Form.Field>
                     <label>Food Item</label>
