cfda96ad1e3725aec617343500ce4fe5923dc672
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2023-09-16 23:25:51 -0500

    changes

--- react-crud/src/components/create.js
+++ react-crud/src/components/create.js
@@ -3,6 +3,7 @@ import { Button, Form } from 'semantic-ui-react'
 
 import foodSubmit from '../handles/foodsubmit';
 import { useRef } from 'react';
+import axios from 'axios';
 //IMPORT FOODSUBMIT
 
 //IMPORT 
@@ -10,6 +11,8 @@ import { useRef } from 'react';
 
 import '../App.css';
 
+const backendUrl = 'http://127.0.0.1:3001/create';
+const backendUrl2 = 'http://127.0.0.1:3001/images';
 
 export default function Create() {
     const foodItem = useRef()
@@ -21,38 +24,47 @@ export default function Create() {
     const [expiry, setExpiry] = useState('');
     const [images, setImages] = useState([]);
     const [imageURLs, setImageURLs] = useState([]);
-    const postData = () => {
-        foodSubmit(food, expiry)
-    }
+    // const postData = async () => {
+    //     try {
+    //         const formData = new FormData();
+
+    //         formData.append('food', food);
+    //         formData.append('expiry', expiry);
+
+    //         images.forEach((image, index) => {
+    //             formData.append(`image${index}`, image);
+    //           });
+            
+    //           const response = await axios.post(backendUrl, formData);
+    //           console.log(response.data);
+    //           alert('Data uploaded successfully');
+    //         } catch (error) {
+    //             console.error(error);   
+    //             alert('Error uploading data');
+    //           }
+
+    //     foodSubmit(food, expiry);
+    // };
+
+    const testSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const requestData = { food: food, expiry: expiry }; // Create an object with the data you want to send
+            const response = await axios.post(backendUrl, requestData);
+          //setMessage(response.data.message);
+        } catch (error) {
+          console.error('Error sending data:', error);
+        }
+      };
 
     useEffect(() => {
         console.log("DID IT");
         if (images.length < 1) return;
         const newImageUrls = [];
-        images.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
+        //images.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
 
         console.log("HEY");
         //new code for backend communication.
-
-        const formData = new FormData();
-        for (let i = 0; i < images.length; i++) {
-            formData.append('image', images[i]);
-        }
-
-        fetch('http://localhost:3000', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            console.log(response);
-            setImageURLs(newImageUrls);
-        })
-        .catch(error => {
-            console.error("Error uploading images: ", error);
-        });
-        console.log("DID IT");
-
-        
 
         setImageURLs(newImageUrls);
         console.log(newImageUrls);
@@ -60,32 +72,33 @@ export default function Create() {
 
     
 
-    // const handleImageUpload = async () => {
-    //     const formData = new FormData();
-    //     for (let i = 0; i < images.length; i++) {
-    //         formData.append('image', images[i]);
-    //     }
-    //     try {
-    //         const response = await fetch('http://localhost:3000/image-urls', {
-    //             method: 'POST',
-    //             body: formData
-    //         });
-    //         console.log(response);
-    //     } catch(error) {
-    //         console.error("Error uploading images: ", error);
-    //     }
-    // };
 
-    function onImageChange(e){
+    // function onImageChange(e){
+    //     console.log(e.target.files[0]);
+    //     setImages([...e.target.files]);
+
+
+    // }
+
+    const onImageUpload = async (e) => {
         setImages([...e.target.files]);
+        
+        axios.post(backendUrl2, formData).then((response) => {
+            console.log(response);
+        })
+        console.log("DONE");
+
+        
     }
+
+    
 
     return (
         <div>
             Upload Food Images
             <br/>
             <br/>
-            <input type="file" multiple accept="image/*" onChange={onImageChange} />
+            <input type="file" multiple accept="image/*" onChange={onImageUpload} />
             {imageURLs.map(imageSrc => <img src={imageSrc}  width="10%"/>)}
             <br/>
             <br/>
@@ -103,7 +116,7 @@ export default function Create() {
                     <label>Expiry Date</label>
                     <input placeholder='Expiry Date' onChange={(e) => setExpiry(e.target.value)}/>
                 </Form.Field>
-                <Button onClick={postData} type='submit'>Submit</Button>
+                <Button onClick={testSubmit} type='submit'>Submit</Button>
             </Form>
         </div>
     )
