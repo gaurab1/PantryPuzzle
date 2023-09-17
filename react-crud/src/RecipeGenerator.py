c202ab26ec545ec631a7e8ecf828f8207106e5bf
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2023-09-17 05:32:22 -0500

    added app and recipe generator to decision mdal

---
+++ react-crud/src/RecipeGenerator.py
@@ -0,0 +1,24 @@
+import re
+import os
+import openai
+
+
+
+def generate_recipe():
+    openai.api_key = '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'
+    #openai.api_key = os.getenv("OPENAI_API_KEY")
+    p = "All using Food, Create a list of recipes, each with a title, description, and ingredients."
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=p,
+        temperature=0.9,
+        max_tokens=2000,
+        top_p=1,
+    )
+    #loop through and print each recipe
+    for i in range(len(response.choices)):
+        print(response.choices[i].text)
+    return response.choices[0].text
+
+if __name__ == "__main__":
+    generate_recipe()
