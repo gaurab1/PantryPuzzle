18e0dd2b83e7d46f992da964ea470bac96a5da1c
Gaurab Das <46531500+gaurab1@users.noreply.github.com>
2023-09-17 09:03:03 -0400

    pics working

--- server/server.py
+++ server/server.py
@@ -4,6 +4,17 @@ sys.path.append('../react-crud/src/')
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from TextFromImage import detectText
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account.
+cred = credentials.Certificate('lol.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+doc_ref = db.collection("test_data").document('random')
 UPLOAD_FOLDER = '../images'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
@@ -31,6 +42,8 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             print(detectText(os.path.join(app.config['UPLOAD_FOLDER'], filename)))
+            food, expiry = detectText(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            doc_ref.set({"food": food, "expirationDate": expiry})
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('upload_file',
                                     filename=filename))
