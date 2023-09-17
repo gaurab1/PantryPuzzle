8245a52bed28758e3c5110c49608c390373ea183
Gaurab Das <46531500+gaurab1@users.noreply.github.com>
2023-09-16 20:51:29 -0400

    Small changes to read.js

--- react-crud/src/components/read.js
+++ react-crud/src/components/read.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Table } from 'semantic-ui-react'
+import { Table, Button } from 'semantic-ui-react'
 
-import { collection, getDocs } from "firebase/firestore"; 
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore"; 
 import { firestore } from '../firebase';
 
 
 const querySnapshot = await getDocs(collection(firestore, "test_data"));
 
+const onDelete = async (id) => {
+    await deleteDoc(doc(firestore, "test_data", id));
+    window.location.reload();
+}
 export default function Read() {
     const date = new Date();
     let day = date.getDate();
@@ -21,17 +25,20 @@ export default function Read() {
                         <Table.HeaderCell>Food Item</Table.HeaderCell>
                         <Table.HeaderCell>Expiry Date</Table.HeaderCell>
                         <Table.HeaderCell>Days to Expiry</Table.HeaderCell>
+                        <Table.HeaderCell>Delete</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
                 {
                 querySnapshot.docs.map(doc => {
+
                     return (
                         <Table.Row>
                            <Table.Cell>{doc.data().food}</Table.Cell>
                             <Table.Cell>{doc.data().expirationDate}</Table.Cell>
-                            <Table.Cell>{Math.ceil((date - new Date(doc.data().expirationDate)) / (1000 * 60 * 60 * 24))}</Table.Cell>
+                            <Table.Cell>{Math.ceil(-(date - new Date(doc.data().expirationDate)) / (1000 * 60 * 60 * 24))}</Table.Cell>
+                            <Table.Cell><Button onClick={() => onDelete(doc.id)}>Delete</Button></Table.Cell>
                          </Table.Row>
                     )
                 })}
